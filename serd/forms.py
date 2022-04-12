6fee88f7f700e8b4cd58a23ef3b75cbd6188f6ad
Christian Vögl <mail@2718-mail.de>
2022-04-12 17:44:10 +0200

    Refactoring [] -> .get

--- serd/forms.py
+++ serd/forms.py
@@ -20,20 +20,20 @@ class OfferForm(forms.ModelForm):
         widget=forms.SelectDateWidget(years=range(2022, 2024))) 
     cost = forms.IntegerField(label=_("Mietpreis"), required=False)
     def clean(self):
-        if not self.cleaned_data['total_number'] > self.cleaned_data['children_number']:
+        if not self.cleaned_data.get('total_number') > self.cleaned_data.get('children_number'):
             self.add_error(field='total_number', error=
             ValueError(
                 _("Mindestens eine Erwachsene Person!")
                 ))
-        if self.cleaned_data['limited_availability']:
-            available_from = self.cleaned_data['available_from']
-            available_until = self.cleaned_data['available_until']
+        if self.cleaned_data.get('limited_availability'):
+            available_from = self.cleaned_data.get('available_from')
+            available_until = self.cleaned_data.get('available_until')
             if (available_until <= available_from):
                 self.add_error(field='available_until', error=
                 ValueError(
                     _("Ende der Verfügbarkeit muss später als der Beginn sein")
                 ))
-        if not self.cleaned_data['seperate_appartment'] and not self.cleaned_data['living_with']:
+        if not self.cleaned_data.get('seperate_appartment') and not self.cleaned_data.get('living_with'):
             self.add_error(field='living_with', error=
             ValueError(
                 _("Eigene Wohnsituation muss angegeben werden, wenn die Unterbringung nicht in einer getrennten Wohnung erfolgt")
@@ -43,7 +43,6 @@ class OfferForm(forms.ModelForm):
             ValueError(
                 _("Bitte Bestätigen dass die Wohnung kostenfrei überlassen wird, oder Preis eingeben")
             ))
-        print(self.cleaned_data.keys())
         if self.cleaned_data.get('for_free') and self.cleaned_data.get('cost'):
             self.add_error(field='cost', error=
                 ValueError(
@@ -78,22 +77,23 @@ class RequestForm(forms.ModelForm):
         'split', 'current_housing', 'representative', 'repr_mail', 'repr_phone', 'arrival_date', 'arrival_location', 'pets', 'pet_number',
         'car', 'languages', 'vaccination', 'accessability_needs')
     def clean(self):
-        pets = self.cleaned_data['pets']
+        pets = self.cleaned_data.get('pets')
         if len(pets) > 1 and 'none' in pets:
             self.add_error(field='pets', error=ValueError(
                 _("Ungültige Auswahl: 'Keine' schließt die anderen Wahlmöglichkeiten aus.")
             ))
         if pets == ['none']:
-            if self.cleaned_data['pet_number']:
+            if self.cleaned_data.get('pet_number'):
                 self.add_error(field='pet_number', error=ValueError(
                     _("Auswahl 'Keine' bei Haustier widerspricht Anzahl ≠ 0")
                 ))
-            else: self.cleaned_data['pet_number'] = 0
-        elif not self.cleaned_data['pet_number']:
+            else:
+                self.cleaned_data['pet_number'] = 0
+        elif not self.cleaned_data.get('pet_number'):
             self.add_error(field='pets', error=ValueError(
                     _("Wenn keine Haustiere vorhanden sind, muss 'Keine' ausgewählt werden.")
                 ))
-        if 'none' not in pets and len(pets) > self.cleaned_data['pet_number']:
+        if 'none' not in pets and len(pets) > self.cleaned_data.get('pet_number'):
             self.add_error(field='pet_number', error=ValueError(
                     _("Mehr Tierarten ausgewählt als Tiere vorhanden")
                 ))
