118d46590257a6226ed08e63f261d46500b86d40
Christian Vögl <mail@2718-mail.de>
2022-04-21 00:23:11 +0200

    allowed hosts for production

--- serd/settings.py
+++ serd/settings.py
@@ -25,7 +25,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-ALLOWED_HOSTS = ['notunterkunft.rgbg-sozial.de','127.0.0.1','0.0.0.0',"192.168.178.34"]
+ALLOWED_HOSTS = ['notunterkunft.rgbg-sozial.de','serd.space-eye.org']
 
 
 # Application definition
