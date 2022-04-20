802e5c66466afe26e7f833b741a5e35b28e516d9
Christian Vögl <mail@2718-mail.de>
2022-04-21 00:20:03 +0200

    Switch of Debug

--- serd/settings.py
+++ serd/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # SECURITY WARNING: keep the secret key used in production secret!
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ['notunterkunft.rgbg-sozial.de','127.0.0.1','0.0.0.0',"192.168.178.34"]
 
