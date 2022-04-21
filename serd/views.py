88d464ce68d292f75c55db7013be2d5e17a2d12e
Christian Vögl <mail@2718-mail.de>
2022-04-21 23:41:42 +0200

    Changed widget for possible_hosts
    The django multipleselect wiget is absolutely unuseable with a few
    hundred objects, therfore I used a django-autocomplete-light widget.
    Sadly this is javascript code

--- serd/views.py
+++ serd/views.py
@@ -10,7 +10,21 @@ from django.views.generic import TemplateView, FormView
 from django.shortcuts import render
 from django.contrib.auth.decorators import login_required
 from .forms import OfferEditForm, OfferForm, RequestEditForm, RequestFilterForm, RequestForm, OfferFilterForm
-import datetime
+from dal import autocomplete
+
+
+class OfferAutocomplete(autocomplete.Select2QuerySetView):
+    def get_queryset(self):
+        print("callled")
+        if not self.request.user.is_authenticated:
+            return Offer.objects.none()
+        qs = Offer.objects.all()
+        if self.q:
+            try:
+                qs = qs.filter(number=int(self.q))
+            except:
+                qs =qs.filter(last_name__istartswith=self.q)
+        return qs
 
 def success(request):
     return HttpResponse("success")
