61df8f62365fa64bea9b14c350bb43b1a1857d68
Christian Vögl <mail@2718-mail.de>
2022-04-27 14:45:10 +0200

    Make hotel in AnsprechpartnerHotel manytomany
    One person may be responsible for multiple hotels, multiple persons for
    one hotel.
    closes #68

--- serd/models.py
+++ serd/models.py
@@ -117,7 +117,7 @@ class AnsprechpartnerHotel(models.Model):
     name = models.CharField(max_length=128, verbose_name="Name")
     tel = models.CharField(max_length=128, validators=[validate_phone], verbose_name="Telefonnummer", blank=True)
     mail = models.CharField(max_length=128, verbose_name="E-mail", validators=[validate_email], blank=True)
-    hotel = models.ForeignKey('Hotel', on_delete=models.SET_NULL, null=True, related_name="ansprechpartner")
+    hotel = models.ManyToManyField('Hotel',  null=True, related_name="ansprechpartner")
     def __str__(self) -> str:
         return "_".join([str(self.number), self.name])
     class Meta:
