728abdf2dd31875ef7b7a3cc5ec7c6df3a867fff
Christian Vögl <mail@2718-mail.de>
2022-04-30 12:06:55 +0200

    room calculations will be moved to the hotel view

--- serd/apps.py
+++
@@ -1,8 +0,0 @@
-from tabnanny import verbose
-from django.apps import AppConfig
-
-class serdConfig(AppConfig):
-    name = 'serd'
-    verbose_name = 'SERD'
-    def ready(self) -> None:
-        from .signals import  delete_handler, save_handler
