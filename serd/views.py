e0b8aede8b8e089ea258811f3cc07357d9353800
Christian Vögl <mail@2718-mail.de>
2022-05-01 23:37:09 +0200

    don't try to save on invalid input

--- serd/views.py
+++ serd/views.py
@@ -57,11 +57,11 @@ def internal_add_housingrequest(request):
             stayset = StaySet(request.POST, prefix='stays')
             if stayset.is_valid() and requestform.is_valid():
                 req = requestform.save()
-            for stayform in stayset:
-                stay = stayform.save(commit=False)
-                stay.request = req
-                stay.save()            
-                return HttpResponseRedirect(reverse('index'))
+                for stayform in stayset:
+                    stay = stayform.save(commit=False)
+                    stay.request = req
+                    stay.save()            
+                    return HttpResponseRedirect(reverse('index'))
             return render(request, 'serd/request_form_intern.html', {'stayset': stayset, 'requestform': requestform})
             
 
